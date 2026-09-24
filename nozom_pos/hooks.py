871336297bc24980779233fd38c1b62d0c0dc6ff
Haitham Salama <haitham@Haithams-MacBook-Air.local>
2026-09-24 21:36:59 +0400

    Fix POS print format fixture migration

--- nozom_pos/hooks.py
+++ nozom_pos/hooks.py
@@ -300,7 +300,6 @@ fixtures = [
                 "name",
                 "in",
                 [
-                    "POS Profile-custom_print_format_2",
                     "Sales Invoice Item-notes",
                     "POS Invoice Item-notes",
                     "Sales Invoice-order_notes",
