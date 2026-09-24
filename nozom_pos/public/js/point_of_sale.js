bdac9248b5fd79013ba75afaaefcb4fb3fee7371
Haitham Salama <haitham@Haithams-MacBook-Air.local>
2026-09-24 17:51:12 +0400

    Fix POS bundle load order

--- nozom_pos/public/js/point_of_sale.js
+++ nozom_pos/public/js/point_of_sale.js
@@ -8,9 +8,11 @@ frappe.pages["point-of-sale"].on_page_load = function (wrapper) {
 		hide_sidebar: true,
 	});
 
-	frappe.require(["point-of-sale.bundle.js", "nozom_pos.bundle.js"], function () {
-		wrapper.pos = new erpnext.PointOfSale.Controller(wrapper);
-		window.cur_pos = wrapper.pos;
+	frappe.require("point-of-sale.bundle.js", function () {
+		frappe.require("nozom_pos.bundle.js", function () {
+			wrapper.pos = new erpnext.PointOfSale.Controller(wrapper);
+			window.cur_pos = wrapper.pos;
+		});
 	});
 };
 
